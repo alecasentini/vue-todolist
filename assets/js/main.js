b97cd6a347bb6834e28bd379214c9b1aae30229b
alecasentini <ale.casentini@gmail.com>
2023-04-20 14:04:24 +0200

    Proprietà true = done = testo sbarrato e funzione aggiungi

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
+        testoInput: '',
         todos: [
             {
                 text: 'Fare i compiti',
@@ -17,10 +18,29 @@ createApp({
                 done: false
             }
         ],
+        error: false,
       
     }
   },
   methods: {
+    creaTodo(){
+
+        if( this.testoInput != '' && this.testoInput.length > 5){
+            this.todos.unshift({text: this.testoInput, done: false})
+            this.testoInput = ''
+            this.error = false
+        } else {
+            this.error = true
+        }
+    },
+
+    cancellareLaTodo(i){
+        this.todos.splice( i, 1 )
+    },
+    
+    toggleDone(index) {
+        this.todos[index].done = !this.todos[index].done;
+    }
 
   }
 }).mount('#app')
